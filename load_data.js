3a8666c6ee6e6fa2185d0d1f5f23174080d68003
ryan-weinstein3 <71529800+ryan-weinstein3@users.noreply.github.com>
2020-12-11 19:03:45 -0500

    Update load_data.js

--- load_data.js
+++ load_data.js
@@ -1,22 +1,9 @@
-const mongodb = require("mongodb").MongoClient;
-const csvtojson = require("csvtojson");
-
-let url = "mongodb+srv://rweinstein:stoneharbor99@cluster0.enwy9.mongodb.net/stocks?retryWrites=true&w=majority"
-
-csvtojson()
-	.fromFile("companies-1.csv")
-	.then(csvData => {
-		console.log(csvData);
-
-	mongodb.connect(url, function(err, db) {
-	  if(err) { return console.log(err); }
-	  
-	    var dbo = db.db("stocks");
-		var collection = dbo.collection('companies');
-		collection.insertMany(csvData, (err, res) => {
-			if (err) throw err;
-			console.log('Inserted: ${res.insertedCount} rows');
-			db.close();
-		});
-	});
-});
+var http = require('http');
+var port = process.env.PORT || 3000;
+console.log("This goes to the console window");
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/html'});
+	res.write("<h2>Hello World<h2>");
+	res.write("Success! This app is deployed online.");
+	res.end();
+}).listen(port);
